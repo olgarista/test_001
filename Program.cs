e07c0eff46018ab66db65f201f7c9743a67bf5c8
olga_aristarkhova <olgaristarkhova@gmail.com>
2022-07-28 23:49:22 +0300

    Метод

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿string[] arr1 = new string [10] {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 string[] arr2 = new string[arr1.Length];
+
+int count = 0;
+void Array(string[] arr1, string[] arr2)
+{
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+Array(arr1, arr2);
